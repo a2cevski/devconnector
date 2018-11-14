10e37ce6dd0b1dfe6e87a34925a5552de01be045
Alex <a2cevski@gmail.com>
2018-11-14 01:26:56 +0100

    Completed user.js route. Going into frondend

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://Alex:ace123123@ds039231.mlab.com:39231/developer-db"
+  mongoURI: "mongodb://Alex:ace123123@ds039231.mlab.com:39231/developer-db",
+  secretOrKey: "TamarsitaBuena"
 };
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+// Load Input Validation
+const validateRegisterInput = require("./validation/register.js");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Database Config
 const db = require("./config/keys.js").mongoURI;
@@ -15,8 +24,13 @@ mongoose
   .connect(db)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
-
 const PORT = process.env.PORT || 5000;
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
 
 // General GET
 app.get("/", (req, res) => {
@@ -27,6 +41,10 @@ app.get("/", (req, res) => {
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+app.get("/*", (req, res) => {
+  res.send("NOPE!");
+});
 
 //App Listen
 app.listen(PORT, () => {
